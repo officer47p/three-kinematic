32f932bebb95be3befa4731a022fabac5812ae3a
Parsa Hosseini <officer47p@gmail.com>
2024-03-17 22:32:13 +0330

    Add multiple balls to the scene per each click

--- src/client/client.ts
+++ src/client/client.ts
@@ -7,7 +7,7 @@ import { GUI } from 'dat.gui'
 const frustumSize = 100
 
 const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(20))
+scene.add(new THREE.AxesHelper(100))
 
 // const camera = new THREE.PerspectiveCamera(
 //     75,
@@ -17,8 +17,10 @@ scene.add(new THREE.AxesHelper(20))
 // )
 // const camera = new THREE.OrthographicCamera(-10, 10,10,-10)
 const aspect = window.innerWidth / window.innerHeight;
-const camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 1000 );
-camera.position.z = 10
+const camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0.1, 1000 );
+camera.position.x = 50
+camera.position.y = 25
+camera.position.z = 100
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -27,16 +29,32 @@ document.body.appendChild(renderer.domElement)
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const controls = new OrbitControls(camera, renderer.domElement)
+// const controls = new OrbitControls(camera, renderer.domElement)
 
-const geometry = new THREE.SphereGeometry(1)
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+class Ball {
+    mesh: THREE.Mesh
+    initPos: THREE.Vector3
+    initVelocity: THREE.Vector3
+    time = 0
+    isLaunched = false
+    constructor(initPos: THREE.Vector3, initVelocity: THREE.Vector3){
+        this.initPos = initPos
+        this.initVelocity = initVelocity
+        const geometry = new THREE.SphereGeometry(1)
+        const material = new THREE.MeshBasicMaterial({
+            color: 0x00ff00,
+            wireframe: true,
+        })
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+        this.mesh = new THREE.Mesh(geometry, material)
+        this.mesh.position.x = initPos.x
+        this.mesh.position.y = initPos.y
+        this.mesh.position.z = initPos.z
+    }
+}
+
+const balls: Ball[] = []
+
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -55,66 +73,28 @@ function onWindowResize() {
 
 
 const gui = new GUI()
-// const cubeFolder = gui.addFolder('Cube')
-// const cubeRotationFolder = cubeFolder.addFolder('Rotation')
-// cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-// cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-// cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-// cubeFolder.open()
-// cubeRotationFolder.open()
-// const cubePositionFolder = cubeFolder.addFolder('Position')
-// cubePositionFolder.add(cube.position, 'x', -10, 10, 2)
-// cubePositionFolder.add(cube.position, 'y', -10, 10, 2)
-// cubePositionFolder.add(cube.position, 'z', -10, 10, 2)
-// cubeFolder.open()
-// cubePositionFolder.open()
-// const cubeScaleFolder = cubeFolder.addFolder('Scale')
-// cubeScaleFolder.add(cube.scale, 'x', -5, 5)
-// cubeScaleFolder.add(cube.scale, 'y', -5, 5)
-// cubeScaleFolder.add(cube.scale, 'z', -5, 5)
-// cubeFolder.add(cube, 'visible')
-// cubeFolder.open()
-// cubeScaleFolder.open()
-
 const options = {
     angle: 70,
     power: 20
 }
-
-
-gui.add(options, "angle", 0, 90)
-gui.add(options, "power", 0, 10)
-
-
-
-let initPos: THREE.Vector3
-let initVelocity: THREE.Vector3
-let isLaunched = false
-
+gui.add(options, "angle", 0, 180)
+gui.add(options, "power", 0, 100)
 
 
 function launch() {
-    timer.reset()
-    initPos = new THREE.Vector3(0,0,0)
-    cube.position.x = initPos.x
-    cube.position.y = initPos.y
-    cube.position.z = 0
-    initVelocity = new THREE.Vector3(Math.cos(degrees_to_radians(options.angle)), Math.sin(degrees_to_radians(options.angle)),0).multiplyScalar(options.power)
-    
-    isLaunched = true
+    console.log("Called launch")
+    // timer.reset()
+    const ball = new Ball(new THREE.Vector3(0,0,0), new THREE.Vector3(Math.cos(degrees_to_radians(options.angle)), Math.sin(degrees_to_radians(options.angle)),0).multiplyScalar(options.power))
+    ball.isLaunched = true
+    scene.add(ball.mesh)
+    balls.push(ball)
 }
-
-
-
-// const clock = new THREE.Clock()
-// clock.getDelta()
 
 
 window.addEventListener('click', (event) => {
     launch()
-}, false)
+}, true)
 
-let time = 0
 
 const timer = new Timer();
 
@@ -123,19 +103,20 @@ function update() {
     // console.log(options)
     // Get Passed Time
     
-    if (isLaunched) {
-        // const _time = clock.getDelta()
-        // const deltaTime = (Date.now() - start) / 10000
-        const delta = timer.getDelta();
-        time += delta
-        // console.log(time)
-
-        const newPosX = kinematicEq(0, initVelocity.x, time, initPos.x)
-        const newPosY = kinematicEq(-9.8, initVelocity.y, time, initPos.y)
-
-        cube.position.x = newPosX
-        cube.position.y = newPosY
-    }
+    balls.forEach(ball => {
+        if (ball.isLaunched) {
+            console.log("dfnjksn")
+            const delta = timer.getDelta();
+            ball.time += delta
+            // console.log(time)
+    
+            const newPosX = kinematicEq(0, ball.initVelocity.x, ball.time, ball.initPos.x)
+            const newPosY = kinematicEq(-9.8, ball.initVelocity.y, ball.time, ball.initPos.y)
+    
+            ball.mesh.position.x = newPosX
+            ball.mesh.position.y = newPosY
+        }
+    })
 
 
     requestAnimationFrame(update)
